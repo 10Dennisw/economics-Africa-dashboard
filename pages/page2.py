fb1a4175e1ddb89ef56e2fca54a9a885699b005f
10DennisW <denniswinkler48@gmail.com>
2024-01-18 16:05:11 +0000

    adding scatter chart and bar chart on page 2

--- pages/page2.py
+++ pages/page2.py
@@ -5,6 +5,7 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 
 
 
@@ -47,35 +48,99 @@ def getting_labels_and_values(filtered_df):
 label_2000_lst, values_2000_lst = getting_labels_and_values(filtered_2000)
 label_2022_lst, values_2022_lst = getting_labels_and_values(filtered_2022)
 
-fig = make_subplots(1, 2, specs=[[{'type':'domain'}, {'type':'domain'}]],
+pie_fig = make_subplots(1, 2, specs=[[{'type':'domain'}, {'type':'domain'}]],
                     subplot_titles=['Year 2000', 'Year 2022'])
-fig.add_trace(go.Pie(labels=label_2000_lst, 
+pie_fig.add_trace(go.Pie(labels=label_2000_lst, 
                      values=values_2000_lst, 
                      scalegroup='one',
                      name="African GDP 2000",
                      marker=dict(colors=[country_colours[label] for label in label_2000_lst])), 
                      1, 1)
-fig.add_trace(go.Pie(labels=label_2022_lst, 
+pie_fig.add_trace(go.Pie(labels=label_2022_lst, 
                      values=values_2022_lst, 
                      scalegroup='one',
                      name="African GDP 2022",
                      marker=dict(colors=[country_colours[label] for label in label_2000_lst])), 
                      1, 2)
-fig.update_layout(title_text="<b>Evolution of African GDP from 2000 to 2022</b>",
+pie_fig.update_layout(title_text="<b>Evolution of African GDP from 2000 to 2022</b>",
                   title=dict(x=0.5),
                   font=dict(color="black")
                   )
-fig.update_traces(marker=dict(line=dict(color='black', width=2)),
+pie_fig.update_traces(marker=dict(line=dict(color='black', width=2)),
                   insidetextfont=dict(color='black', family="Arial", size=16))
 
 
+country_lst = ['South Africa', 'Nigeria', 'Egypt', 'Algeria', 'Morocco']
+# Using boolean indexing to filter rows
+filtered_df = df[df['Country'].isin(country_lst)]
+
+scatter_fig = go.Figure()
+
+# iterating over countries in list and creating a values for each country
+for country in filtered_df['Country'].unique():
+    country_data = filtered_df[filtered_df['Country'] == country]
+    scatter_fig.add_trace(go.Scatter(
+        x=country_data['Year'],
+        y=country_data['GDP (USD)'],
+        name=country  # creating legend label
+    ))
+
+# Updating layout of the scart
+scatter_fig.update_layout(
+    title='GDP over Time',
+    xaxis_title='Year',
+    yaxis_title='GDP (USD)',
+    showlegend=True, 
+)
+
+# function to find the index of a word (country in this case) in a list
+def finding_index (word, list):
+    other_index = None
+    for i in range(len(list)):
+        if list[i] == word:
+            other_index = i
+    return other_index
+
+# finding an removing other in the list 
+other_index_2000 = finding_index('Other', label_2000_lst)
+label_2000_lst.remove(label_2000_lst[other_index_2000])
+values_2000_lst.remove(values_2000_lst[other_index_2000])
+
+# finding an removing other in the list 
+other_index_2022 = finding_index('Other', label_2022_lst)
+label_2022_lst.remove(label_2022_lst[other_index_2022])
+values_2022_lst.remove(values_2022_lst[other_index_2022])
+
+bar_fig = go.Figure(data=[
+    go.Bar(name='2000', x=label_2000_lst, y=values_2000_lst),
+    go.Bar(name='2022', x=label_2022_lst, y=values_2022_lst)
+])
+
+# Changing the bar mode to group them together
+bar_fig.update_layout(barmode='group')
+
 # Defining layout for Page 2 with a bar chart
 layout = html.Div([
-   
-    dcc.Graph(figure=fig,
-              id='african-gdp-graph',
-              style={'border': '1px solid black', 'height': '350px', 'margin-left': '1px', 'margin-right': '1px', 'margin-top': '1px', 'margin-bottom': '1px', 'backgroundColor': '#000000'},
-              )
+    html.Div(style={'display': 'flex', 'backgroundColor': 'white'}, children=[
+        dcc.Graph(figure=pie_fig,
+                id='african-gdp-graph',
+                style={'border': '1px solid black', 'height': '375px', 'width': '100%', 'margin-left': '5px', 'margin-right': '5px', 'margin-top': '1px', 'margin-bottom': '1px', 'backgroundColor': '#000000'},
+                )
+    ]),
+
+    html.Div(style={'display': 'flex', 'backgroundColor': 'white'}, children=[   
+        # Setting the format for the line chart
+        dcc.Graph(figure=scatter_fig,
+            id='scatter-chart',
+            style={'border': '1px solid black', 'height': '375px', 'width': '49%', 'float': 'left','margin-left': '5px', 'margin-right': '10px','margin-top': '5px', 'margin-bottom': '1px', 'backgroundColor': '#000000'}       
+        ),
+
+        # Setting the format for the bar chart
+        dcc.Graph(figure=bar_fig,
+            id='bar-chart',
+            style={'border': '1px solid black', 'height': '375px', 'width': '49%', 'float': 'right', 'margin-top': '5px', 'margin-right': '5px', 'margin-bottom': '1px', 'backgroundColor': '#000000'}
+        ),
+    ]),
 ])
 
 '''
