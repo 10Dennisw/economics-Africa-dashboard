a90b11ea656055101a6449ff20a86a4fc8cdb6e6
10DennisW <denniswinkler48@gmail.com>
2024-02-08 19:51:44 +0000

    editing page 3, through callbacks

--- pages/page3.py
+++ pages/page3.py
@@ -14,93 +14,41 @@ import matplotlib.pyplot as plt
 dash.register_page(__name__, path='/Page3', name="Africa: GDP per Capita")
 
 ############################################################################################################
-# Loadung t=
+# Loading data
 url = "https://raw.githubusercontent.com/10Dennisw/visualisations/master/africa_economics_v2.csv"
 df = pd.read_csv(url)
-df= df.loc[df['Year']==2000]
-
-############################################################################################################
-# Creating a map figure
-
-map_fig = px.choropleth(
-    df,
-    locations='Code',
-    color= np.log(df['GDP per Capita']),
-    hover_name='Country',
-    color_continuous_scale='reds',
-    projection='orthographic',
-    title='',
-    template='plotly',
-    range_color=[min(np.log(df['GDP per Capita'])), max(np.log(df['GDP per Capita']))]
-)
-
-############################################################################################################
-# Creating Histogram figure
-
-hist_fig = px.histogram(df, x='GDP per Capita', nbins=50, title='GDP per Capita Distribution', labels={'GDP per Capita': 'GDP per Capita', 'count': 'Frequency'})
-
-############################################################################################################
-# Creating bar chart
-
-# defining average value
-average_val = df['GDP per Capita'].mean()
-# filtering and sorting the dataframe to show the top 10 values in order
-top_10 = df.nlargest(10, 'GDP per Capita')
-top_10 = top_10.sort_values(by=['GDP per Capita'], ascending=True)
-
-# creating a horizontal bar plot
-bar_fig = go.Figure(go.Bar(
-    y=top_10['Country'],
-    x=top_10['GDP per Capita'],
-    orientation='h'
-))
-
-# adding a vertical line to show average GDP per Capita
-bar_fig.add_shape(
-    type="line",
-    x0=average_val,
-    y0=0,
-    x1=average_val,
-    y1=len(top_10),
-    line=dict(
-        color="black",
-        width=3,
-        dash="dashdot"
-        ),
-    name='Average GDP per Capita'
-    )
-
-# adding titles and axis labels
-bar_fig.update_layout(
-    title='GDP per Capita',
-    yaxis=dict(title='Country'),
-    xaxis=dict(title='GDP per Capita')
-)
-
+filtered_df_map=df[df['Year']==2000]
 
 ############################################################################################################
 # Defining layout for Page 3 with a bar chart
 layout = html.Div([
+    dcc.Slider(
+        id='year-slider-page-three',
+        min=df['Year'].min(),
+        max=df['Year'].max(),
+        value=df['Year'].min(), # setting the default value
+        marks={str(year): str(year) for year in range(df['Year'].min(), df['Year'].max() + 1)},
+        step=1
+    ),
+
     html.Div(style={'display': 'flex', 'backgroundColor': 'white'}, children=[
-        dcc.Graph(figure=map_fig,
-                id='gdp-per-capita-graph',
+        dcc.Graph(
+                  id='gdp-per-capita-graph',
                 style={'border': '1px solid black', 'height': '375px', 'width': '100%', 'margin-left': '5px', 'margin-right': '5px', 'margin-top': '1px', 'margin-bottom': '1px', 'backgroundColor': '#000000'},
                 )
     ]),
 
     html.Div(style={'display': 'flex', 'backgroundColor': 'white'}, children=[   
         # Setting the format for the line chart
-        dcc.Graph(figure=hist_fig,
-            id='histogram-chart',
-            style={'border': '1px solid black', 
-                   'height': '375px', 'width': '49%', 
-                   'float': 'left',
-                   'margin-left': '5px', 'margin-right': '10px','margin-top': '5px', 'margin-bottom': '1px', 
-                   'backgroundColor': '#000000'}       
+        dcc.Graph(id='histogram-chart',
+                  style={'border': '1px solid black', 
+                         'height': '375px', 'width': '49%', 
+                         'float': 'left',
+                         'margin-left': '5px', 'margin-right': '10px','margin-top': '5px', 'margin-bottom': '1px', 
+                         'backgroundColor': '#000000'}       
         ),
         # Setting the format for the bar chart
-        dcc.Graph(figure=bar_fig,
-            id='bar-chart',
+        dcc.Graph(id='bar-chart',
             style={'border': '1px solid black', 
                    'height': '375px', 'width': '49%', 
                    'float': 'right', 'margin-top': '5px', 'margin-right': '5px', 'margin-bottom': '1px', 
@@ -108,3 +56,83 @@ layout = html.Div([
         ),
     ]),
 ])
+
+# callack used to create interactivity between the user (through the slider)
+@callback(
+    [Output('gdp-per-capita-graph', 'figure'),
+     Output('histogram-chart', 'figure'),
+     Output('bar-chart', 'figure')],
+    [Input('year-slider-page-three', 'value')],
+    allow_duplicate=True
+)
+
+def update_charts(selected_year):
+    # filter the df based upon the year selected by the user on the slider
+    filtered_df = df.loc[df['Year'] == selected_year]
+
+    map_fig = px.choropleth(
+    filtered_df_map,
+    locations='Code',
+    color= np.log(filtered_df['GDP per Capita']),
+    hover_name='Country',
+    color_continuous_scale='reds',
+    projection='orthographic',
+    title=f'<b>Map of GDP per Capita in {selected_year}</b>',
+    template='plotly',
+    range_color=[min(np.log(filtered_df['GDP per Capita'])), max(np.log(filtered_df['GDP per Capita']))]
+    )
+
+    # setting the title to be in the middle of the figure
+    map_fig.update_layout(title_x=0.5)
+    map_fig.update_geos(projection_rotation=dict(lon=17, lat=0)) # setting the map to focus on Africa
+
+    ############################################################################################################
+    # Creating Histogram figure
+    hist_fig = px.histogram(filtered_df, 
+                            x='GDP per Capita', 
+                            nbins=50, 
+                            title=f'<b>Top 10 Economies (GDP per Capita) in {selected_year}</b>', 
+                            labels={'GDP per Capita': 'GDP per Capita', 'count': 'Frequency'})
+
+    hist_fig.update_layout(title_x=0.5) # setting the title to be in the middle of the figure
+
+    ############################################################################################################
+    # Creating bar chart
+
+    # defining average value
+    average_val = filtered_df['GDP per Capita'].mean()
+    # filtering and sorting the dataframe to show the top 10 values in order
+    top_10 = filtered_df.nlargest(10, 'GDP per Capita')
+    top_10 = top_10.sort_values(by=['GDP per Capita'], ascending=True)
+
+    # creating a horizontal bar plot
+    bar_fig = go.Figure(go.Bar(
+        y=top_10['Country'],
+        x=top_10['GDP per Capita'],
+        orientation='h'
+    ))
+
+    # adding a vertical line to show average GDP per Capita
+    bar_fig.add_shape(
+        type="line",
+        x0=average_val,
+        y0=0,
+        x1=average_val,
+        y1=len(top_10),
+        line=dict(
+            color="black",
+            width=3,
+            dash="dashdot"
+            ),
+        name='Average GDP per Capita'
+        )
+
+    # adding titles and axis labels
+    bar_fig.update_layout(
+        title=f'<b>Top 10 Economies (GDP per Capita)</b>',
+        title_x=0.5,
+        yaxis=dict(title='Country'),
+        xaxis=dict(title='GDP per Capita'),
+    )
+
+    return map_fig, hist_fig, bar_fig
